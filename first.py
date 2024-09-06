0c90e7ad1baf859ef165e1d9b3ebcacbbc5630ca
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-06 11:56:37 +0300

    Merge remote-tracking branch 'origin/master'

--- first.py
+++ first.py
@@ -2,5 +2,5 @@ name = "Gayatriben Bhavsar"
 print(f"Hello, {name}!")
 print(f"Hello, {name}!")
 print(f"Hello, {name}!")
-print(f"Hello, {name}!")
+print(f"Hello, Kirpal")
 print(f"good morning,{name}")

22c21e704c0877d3f66c0559eca6c8e02f0883c4
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-21 18:29:17 +0300

    at 18:29

--- first.py
+++ first.py
@@ -1,57 +1,18 @@
-import random
-num_dice = int(input("How many dice would you like to roll? "))
-
-total_sum = 0
-for _ in range(num_dice):
-    roll = random.randint(1, 6)
-    total_sum += roll
-
-print(f"The sum of the rolls is: {total_sum}")
-
-numbers = []
+seasons = ("winter", "spring", "summer", "autumn")
 
 
-while True:
-    user_input = input("Enter a number (or press Enter to finish): ")
-    if user_input == '':
-        break
-    else:
-        try:
-            numbers.append(int(user_input))
-        except ValueError:
-            print("Please enter a valid number.")
+month = int(input("Enter the number of the month (1-12): "))
 
 
-numbers.sort(reverse=True)
-top_5 = numbers[:5]
-print("The top 5 greatest numbers are:", top_5)
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-number = int(input("Enter an integer: "))
-
-
-if is_prime(number):
-    print(f"{number} is a prime number.")
+if month in (12, 1, 2):
+    season = seasons[0]  # winter
+elif month in (3, 4, 5):
+    season = seasons[1]  # spring
+elif month in (6, 7, 8):
+    season = seasons[2]  # summer
+elif month in (9, 10, 11):
+    season = seasons[3]  # autumn
 else:
-    print(f"{number} is not a prime number.")
+    season = "Invalid month number."
 
-
-    cities = []
-
-
-    for i in range(5):
-        city = input(f"Enter the name of city {i + 1}: ")
-        cities.append(city)
-
-
-    print("\nThe cities you entered are:")
-    for city in cities:
-        print(city)
+print(f"The season is: {season}")

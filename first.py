677a1946b5e4dc714cf0366782e205ba79f05aee
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-21 18:08:31 +0300

    at 18:08

--- first.py
+++ first.py
@@ -7,3 +7,21 @@ for _ in range(num_dice):
     total_sum += roll
 
 print(f"The sum of the rolls is: {total_sum}")
+
+numbers = []
+
+
+while True:
+    user_input = input("Enter a number (or press Enter to finish): ")
+    if user_input == '':
+        break
+    else:
+        try:
+            numbers.append(int(user_input))
+        except ValueError:
+            print("Please enter a valid number.")
+
+
+numbers.sort(reverse=True)
+top_5 = numbers[:5]
+print("The top 5 greatest numbers are:", top_5)

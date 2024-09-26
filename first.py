ddef85c141dec467183b04680d02a1c6c2bb4ff7
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-26 12:12:58 +0300

    Function question-1

--- first.py
+++ first.py
@@ -1,17 +1,14 @@
 import random
 
-def calculate_pi(num_points):
-    inside_circle = 0
 
-    for _ in range(num_points):
-        x = random.uniform(-1, 1)
-        y = random.uniform(-1, 1)
-        if x**2 + y**2 < 1:
-            inside_circle += 1
+def roll_dice():
+    return random.randint(1, 6)
 
-    pi_approx = 4 * inside_circle / num_points
-    return pi_approx
 
-num_points = int(input("Enter the number of random points to generate: "))
-approx_pi = calculate_pi(num_points)
-print(f"Approximation of pi: {approx_pi}")
+def main():
+    result = 0
+    while result != 6:
+        result = roll_dice()
+        print(f"Dice rolled: {result}")
+
+main()

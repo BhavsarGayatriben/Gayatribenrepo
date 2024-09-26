d082ded7b8f8b4509b4b5f289dee94cfd8a03e2e
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-26 10:39:28 +0300

    qustion-1

--- first.py
+++ first.py
@@ -1,8 +1,5 @@
-
-while True:
-    inches = float(input("Enter inches (negative value to stop): "))
-    if inches < 0:
-        print("Program ended.")
-        break
-    centimeters = inches * 2.54
-    print(f"{inches} inches is {centimeters} centimeters.")
+i = 1
+while i <= 1000:
+    if i % 3 == 0:
+        print(i)
+    i += 1

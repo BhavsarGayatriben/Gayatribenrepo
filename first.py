3b2996b11dfa4c572e43014a0e5b485c23088662
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-27 10:10:41 +0300

    Function question-4

--- first.py
+++ first.py
@@ -1,8 +1,6 @@
-# Function to calculate the sum of integers in a list
 def sum_of_list(numbers):
     return sum(numbers)
 
-# Main program: create a list and call the sum function
 def main():
     numbers = [1, 2, 3, 4, 5]
     total = sum_of_list(numbers)

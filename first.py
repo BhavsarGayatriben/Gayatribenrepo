4e0004a440b87f0c3faf2d406c4fea22e407d1e4
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-09-27 10:12:02 +0300

    Function question-5

--- first.py
+++ first.py
@@ -1,9 +1,10 @@
-def sum_of_list(numbers):
-    return sum(numbers)
+def remove_odd_numbers(numbers):
+    return [num for num in numbers if num % 2 == 0]
 
 def main():
-    numbers = [1, 2, 3, 4, 5]
-    total = sum_of_list(numbers)
-    print(f"The sum of the list {numbers} is {total}")
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    even_numbers = remove_odd_numbers(numbers)
+    print(f"Original list: {numbers}")
+    print(f"List after removing odd numbers: {even_numbers}")
 
 main()

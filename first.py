284a82329332733f2203f1f27777538ae9a14477
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-10-03 18:13:46 +0300

    Car race simulation with 10 cars

--- first.py
+++ first.py
@@ -1,3 +1,7 @@
+
+
+import random
+
 class Car:
     def __init__(self, registration_number, max_speed):
         self.registration_number = registration_number
@@ -16,10 +20,24 @@ class Car:
         self.travelled_distance += self.current_speed * hours
 
     def __str__(self):
-        return f"Registration Number: {self.registration_number}, Max Speed: {self.max_speed} km/h, Current Speed: {self.current_speed} km/h, Travelled Distance: {self.travelled_distance} km"
+        return f"{self.registration_number:<10} {self.max_speed:<15} {self.current_speed:<15} {self.travelled_distance:<15}"
 
-# Main program
-car = Car("ABC-123", 142)
-car.accelerate(60)  # Speed up to 60 km/h
-car.drive(1.5)      # Drive for 1.5 hours
-print(f"After driving: {car}")
+
+cars = []
+for i in range(1, 11):
+    registration_number = f"ABC-{i}"
+    max_speed = random.randint(100, 200)
+    cars.append(Car(registration_number, max_speed))
+
+race_ongoing = True
+while race_ongoing:
+    for car in cars:
+        speed_change = random.randint(-10, 15)
+        car.accelerate(speed_change)
+        car.drive(1)
+        if car.travelled_distance >= 10000:
+            race_ongoing = False
+
+print(f"{'Registration':<10} {'Max Speed (km/h)':<15} {'Current Speed (km/h)':<15} {'Travelled Distance (km)':<15}")
+for car in cars:
+    print(car)

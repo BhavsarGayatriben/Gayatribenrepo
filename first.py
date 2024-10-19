985f21c16447faf4f5e4cea33e32ef0fb385e433
BhavsarGayatriben <GayatribenVivek.Bhavsar@metropolia.fi>
2024-10-19 14:28:20 +0300

    Writing, reading, and appending notes to a file

--- first.py
+++ first.py
@@ -1,33 +1,17 @@
+def find_season():
+    seasons = ("Winter", "Spring", "Summer", "Autumn")
+    month = int(input("Enter the number of the month (1-12): "))
+
+    if month in [12, 1, 2]:
+        print("The season is:", seasons[0])
+    elif month in [3, 4, 5]:
+        print("The season is:", seasons[1])
+    elif month in [6, 7, 8]:
+        print("The season is:", seasons[2])
+    elif month in [9, 10, 11]:
+        print("The season is:", seasons[3])
+    else:
+        print("Invalid month number!")
 
 
-def write_notes(file_name):
-    with open(file_name, "w") as file:
-        note = input("Write a new note: ")
-        file.write(note + "\n")
-
-def read_notes(file_name):
-    with open(file_name, "r") as file:
-        notes = file.readlines()
-        for note in notes:
-            print(note.strip())
-
-def append_notes(file_name):
-    with open(file_name, "a") as file:
-        note = input("Write additional note to append: ")
-        file.write(note + "\n")
-
-file_name = "notes.txt"
-
-# Write a new note
-write_notes(file_name)
-
-# Read existing notes
-print("\nReading notes from the file:")
-read_notes(file_name)
-
-# Append new notes
-append_notes(file_name)
-
-# Read updated notes
-print("\nUpdated notes in the file:")
-read_notes(file_name)
+find_season()
